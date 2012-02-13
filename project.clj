f1017d7fd215bb06963a99ce7ff36456dcb4afa6
Nikita Zubkov <zubchick@yandex-team.ru>
2012-02-14 01:56:13 +0400

    sim-distance

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject pci "0.0.1"
   :description "Examples for \"Programming Collective Intelligence\""
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/math.numeric-tower "0.0.1"]]
+  :dev-dependencies [[swank-clojure "1.3.4-SNAPSHOT"]])
--- src/pci/ch1/recommendations.clj
+++ src/pci/ch1/recommendations.clj
@@ -1,4 +1,6 @@
-(ns pci.ch1.recommendations)
+(ns pci.ch1.recommendations
+  (:require clojure.set
+            [clojure.math.numeric-tower :as math]))
 
 (def critics
   {"Lisa Rose" {"Lady in the Water" 2.5, "Snakes on a Plane" 3.5
@@ -20,3 +22,19 @@
                      "You, Me and Dupree" 3.5},
    "Toby" {"Snakes on a Plane" 4.5, "You, Me and Dupree" 1.0
             "Superman Returns" 4.0}})
+
+
+(defn sum-of-squares [x y]
+  (math/expt (- x y) 2))
+
+(defn sim-distance [prefs person1 person2]
+  (let [pref1 (prefs person1)
+        pref2 (prefs person2)
+        keys-pref1 (keys pref1)
+        keys-pref2 (keys pref2)]
+    (if (count (clojure.set/intersection (set keys-pref1)
+                                         (set keys-pref2)))
+      (let [sum-of-sqr (apply + (for [item keys-pref1 :when (pref2 item)]
+                                  (sum-of-squares (pref1 item) (pref2 item))))]
+        (/ 1 (+ 1 sum-of-sqr)))
+      0)))
