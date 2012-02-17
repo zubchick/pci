c2d4d084d3542fa20a1fe9769c2f8acf499cdccd
Nikita Zubkov <zubchick@yandex-team.ru>
2012-02-18 01:57:31 +0400

    get-recommendations

--- src/pci/ch1/recommendations.clj
+++ src/pci/ch1/recommendations.clj
@@ -3,25 +3,25 @@
             [clojure.math.numeric-tower :as math]))
 
 (def critics
-  {"Lisa Rose" {"Lady in the Water" 2.5, "Snakes on a Plane" 3.5
-                 "Just My Luck" 3.0, "Superman Returns" 3.5
-                 "You, Me and Dupree" 2.5, "The Night Listener" 3.0}
-   "Gene Seymour" {"Lady in the Water" 3.0, "Snakes on a Plane" 3.5
-                    "Just My Luck" 1.5, "Superman Returns" 5.0
-                    "The Night Listener" 3.0, "You, Me and Dupree" 3.5}
-   "Michael Phillips" {"Lady in the Water" 2.5, "Snakes on a Plane" 3.0
-                        "Superman Returns" 3.5, "The Night Listener" 4.0}
-   "Claudia Puig" {"Snakes on a Plane" 3.5, "Just My Luck" 3.0
-                    "The Night Listener" 4.5, "Superman Returns" 4.0
-                    "You, Me and Dupree" 2.5}
-   "Mick LaSalle" {"Lady in the Water" 3.0, "Snakes on a Plane" 4.0
-                    "Just My Luck" 2.0, "Superman Returns" 3.0
-                    "The Night Listener" 3.0, "You, Me and Dupree" 2.0}
-   "Jack Matthews" {"Lady in the Water" 3.0, "Snakes on a Plane" 4.0
-                     "The Night Listener" 3.0, "Superman Returns" 5.0
-                     "You, Me and Dupree" 3.5},
-   "Toby" {"Snakes on a Plane" 4.5, "You, Me and Dupree" 1.0
-            "Superman Returns" 4.0}})
+  {"Lisa Rose" {"Lady in the Water" 5/2, "Snakes on a Plane" 7/2
+                 "Just My Luck" 3, "Superman Returns" 7/2
+                 "You, Me and Dupree" 5/2, "The Night Listener" 3}
+   "Gene Seymour" {"Lady in the Water" 3, "Snakes on a Plane" 7/2
+                    "Just My Luck" 3/2, "Superman Returns" 5
+                    "The Night Listener" 3, "You, Me and Dupree" 7/2}
+   "Michael Phillips" {"Lady in the Water" 5/2, "Snakes on a Plane" 3
+                        "Superman Returns" 7/2, "The Night Listener" 4}
+   "Claudia Puig" {"Snakes on a Plane" 7/2, "Just My Luck" 3
+                    "The Night Listener" 9/2, "Superman Returns" 4
+                    "You, Me and Dupree" 5/2}
+   "Mick LaSalle" {"Lady in the Water" 3, "Snakes on a Plane" 4
+                    "Just My Luck" 2, "Superman Returns" 3
+                    "The Night Listener" 3, "You, Me and Dupree" 2}
+   "Jack Matthews" {"Lady in the Water" 3, "Snakes on a Plane" 4
+                     "The Night Listener" 3, "Superman Returns" 5
+                     "You, Me and Dupree" 7/2},
+   "Toby" {"Snakes on a Plane" 9/2, "You, Me and Dupree" 1
+            "Superman Returns" 4}})
 
 (defn sum-of-squares [x y]
   (math/expt (- x y) 2))
@@ -43,22 +43,20 @@
       0)))
 
 (defn sim-pearson [prefs person1 person2]
-  (let[pref1 (prefs person1)
-       pref2 (prefs person2)
-       keys-pref1 (keys pref1)
-       keys-pref2 (keys pref2)
-       ;; общие интересы
-       si (clojure.set/intersection (set keys-pref1)
-                                    (set keys-pref2))
-       n (count si)]
+  (let [pref1 (prefs person1)
+        pref2 (prefs person2)
+        keys-pref1 (keys pref1)
+        keys-pref2 (keys pref2)
+        ;; общие интересы
+        si (clojure.set/intersection (set keys-pref1)
+                                     (set keys-pref2))
+        n (count si)]
 
     (if (= 0 n)
       0
-      (let [big-table (for [item si] (let [i1 (pref1 item)
-                                           i2 (pref2 item)]
-                                       [i1, i2
-                                        (math/expt i1 2), (math/expt i2 2)
-                                        (* i1 i2)]))
+      (let [big-table (for [item si :let [i1 (pref1 item)
+                                          i2 (pref2 item)]]
+                        [i1, i2, (math/expt i1 2), (math/expt i2 2), (* i1 i2)])
             zip-table (apply zip big-table)
             sums-table (map #(apply + %) zip-table)
             [sum1 sum2 sum1sq sum2sq psum] sums-table
@@ -74,3 +72,34 @@
   (let [scores (for [other (keys prefs) :when (not= other person)]
                  [(similarity prefs person other) other])]
     (-> (sort-by first scores) reverse vec (subvec 0 n))))
+
+
+(defn group-sum
+  "(group-sum [[:a 1] [:b 2] [:a 1] [:b 2]])
+   {:b 4, :a 2}"
+  [kv-seq]
+  (reduce (fn [result item]
+            (let [[key value] item
+                  total-sum (or (result key) 0)]
+              (assoc result key (+ total-sum value))))
+          {}
+          kv-seq))
+
+(defn get-recommendations [prefs person &
+                           {:keys [similarity] :or {similarity sim-pearson}}]
+  (let [totals-and-sums
+        (for [other (keys prefs) :when (not= other person)
+              :let [sim (similarity prefs person other)]
+              :when (> sim 0)]
+          (for [item (keys (prefs other)) :let [rating ((prefs person) item)]
+                :when (or (nil? rating)
+                          (zero? rating))]
+            [[item, (* sim ((prefs other) item))]
+             [item, sim]]))
+
+        prepare (fn [getter] (reduce concat []
+                                    (map #(map getter %) totals-and-sums)))
+        totals (group-sum (prepare first))
+        sim-sums (group-sum (prepare second))
+        rankings (for [[item, total] totals] [(/ total (sim-sums item)), item])]
+    (-> rankings sort reverse)))
