4e887253f23bc86e3b6e80c9ee8c59294f5086b3
Nikita Zubkov <zubchick@yandex-team.ru>
2012-02-19 02:24:32 +0400

    update config

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Examples for \"Programming Collective Intelligence\""
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]]
-  :dev-dependencies [[swank-clojure "1.3.4-SNAPSHOT"]])
+  :dev-dependencies [[clojure-source "1.3.0"]]
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
