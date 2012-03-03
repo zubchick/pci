3ba62bca04a9b7df8655a08172bc07474595118a
Nikita Zubkov <zubchick@gmail.com>
2012-03-03 20:53:15 +0400

    fisher-classify

--- src/pci/ch6/docclass.clj
+++ src/pci/ch6/docclass.clj
@@ -1,6 +1,7 @@
 (ns pci.ch6.docclass
   (:require [clojure.math.numeric-tower :as math]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import [java.lang Math]))
 
 (defn get-words [doc]
   (let [words (for [word (str/split doc #"\W")
@@ -14,6 +15,11 @@
   {:cc (atom {}), :fc (atom {})
    :get-features get-features
    :thresholds (atom {})})
+
+(defn create-fclassifier [get-features]
+  {:cc (atom {}), :fc (atom {})
+   :get-features get-features
+   :minimums (atom {})})
 
 (defn- fc-inc [fc f cat]
   (if-let [pairs (fc f)]
@@ -75,7 +81,7 @@
          count)
       0)))
 
-(defn weight-prob [classifier f cat prf &
+(defn weighted-prob [classifier f cat prf &
                               {:keys [weight ap] :or {weight 1 ap 1/2}}]
   (let [basic-prob (prf classifier f cat)
         totals (apply + (for [c (categories classifier)]
@@ -88,7 +94,7 @@
         features (get-f item)]
     (apply *
            (for [[f _] features]
-             (weight-prob classifier f cat fprob)))))
+             (weighted-prob classifier f cat fprob)))))
 
 (defn prob [classifier item cat]
   (let [catprob (/ (cat-count classifier cat)
@@ -106,12 +112,24 @@
       score
       1)))
 
+
+(defn set-minimum! [fclassifier cat min]
+  (let [mins (fclassifier :minimums)]
+    (swap! mins assoc cat min)))
+
+(defn get-minimum [fclassifier cat]
+  (let [mins (fclassifier :minimums)]
+    (if-let [score (@mins cat)]
+      score
+      0)))
+
+
 (defn classify [classifier item &
                 {:keys [default] :or {default :unknown}}]
   (loop [cats (categories classifier)
          probs {}
          max 0
-         best nil]
+         best default]
     (if (seq cats)
       (let [cat (first cats)
             candidate (prob classifier item cat)
@@ -129,3 +147,48 @@
                 default
                 (recur (rest (seq sq))))))
           best)))))
+
+(defn fclassify [fclassifier item &
+                 {:keys [default] :or {default :unknown}}]
+  (loop [cats (categories fclassifier)
+         max 0
+         best default]
+    (if (seq cats)
+      (let [c (first cats)
+            p (fisher-prob fclassifier item c)]
+        (if (and (> p (get-minimum fclassifier c))
+                 (> p max))
+          (recur (rest cats) p c)
+          (recur (rest cats) max best)))
+      best)))
+
+
+(defn cprob [classifier f cat]
+  (let [clf (fprob classifier f cat)]
+    (if (zero? clf)
+      0
+      (let [freq (for [c (categories classifier)]
+                   (fprob classifier f c))]
+        (/ clf (apply + freq))))))
+
+
+(defn invchi2 [chi df]
+  (let [m (/ chi 2)
+        exp (Math/exp (- m))]
+    (loop [sum exp, term exp, i 1]
+      (if (< i (int (/ df 2)))
+        (let [new-term (* term (/ m i))
+              new-sum (+ sum new-term)]
+          (recur new-sum new-term (inc i)))
+        (min sum 1)))))
+
+
+(defn fisher-prob [classifier item cat]
+  (let [get-f (classifier :get-features)
+        features (get-f item)
+        p (apply *
+                 (for [[f _] features]
+                   (weighted-prob classifier f cat cprob)))
+        fscore (* -2 (Math/log p))]
+    (invchi2 fscore (* 2 (count features)))))
+
